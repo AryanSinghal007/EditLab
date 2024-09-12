157800ba78127ebcabbfcffd85bf35c5856a0b46
Aryan Singhal <aryansinghal237@gmail.com>
2024-09-13 02:45:00 +0530

    express server for web-sockets

--- src/Pages/EditorPage.js
+++ src/Pages/EditorPage.js
@@ -1,8 +1,25 @@
-import react, {useState} from 'react';
+import react, {useState, useRef, useEffect} from 'react';
 import Client from '../components/Client';
 import Editor from '../components/Editor';
+import { initSocket } from '../socket';
+// import ACTIONS from '../Actions';
+import { useLocation } from 'react-router-dom';
 
 const EditorPage = () => {
+
+    const socketRef = useRef(null);
+    const location = useLocation();
+
+    useEffect(() => {
+        const init = async () => {
+            socketRef.current = await initSocket();
+            // socketRef.current.emit(ACTIONS.JOIN, {
+            //     roomId,
+            //     username: location.state?.username,
+            // });
+        };
+        init();
+    });
 
     const [clients, setClients] = useState([
         { socketId : 1, username : 'Yash Gupta' },
