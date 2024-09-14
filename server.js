e60e1a5ce58ab52b7f77593ce6f9118c74f0c60c
Aryan Singhal <aryansinghal237@gmail.com>
2024-09-15 04:20:09 +0530

    vercel config v2

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use(cors({
 
 
 app.use(express.static('build'));
-app.use((req, res, next) => {
+app.use('/', (req, res, next) => {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 })
 
